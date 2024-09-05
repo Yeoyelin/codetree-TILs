aa869fcc1f8f6f212744efdfbf78dd191f4ef730
Yeoyelin <yeoirene0130@gmail.com>
2024-09-05 11:52:13 +0900

    Title: 100점 도달하기, Time: 216ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240905/100점 도달하기/reach-100-point.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+k = 100 - n
+
+for i in range(k+1):
+    if n>= 90:
+        print("A", end = " ")
+    elif n>= 80:
+        print("B", end = " ")
+    elif n>= 70:
+        print("C", end = " ")
+    elif n>= 60:
+        print("D", end = " ")
+    else:
+        print("F", end = " ")
+    n += 1
