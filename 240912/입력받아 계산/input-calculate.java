8b8972799de48e52d933e48c13b153dd09ec3573
Yeoyelin <yeoirene0130@gmail.com>
2024-09-12 00:17:33 +0900

    Title: 입력받아 계산, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240912/입력받아 계산/input-calculate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /* Scanner인 객체 sc를 만든다 = 객체 sc는 시스템이 입력을 받을 수 있는 새로운 Scanner*/
+        int a = sc.nextInt();
+        System.out.println(a+2);
+    }
+}
