e02a28effb449ff808fe1c94ca035568615bd071
Yeoyelin <yeoirene0130@gmail.com>
2025-01-21 17:10:57 +0900

    Title: 특정 위치의 문자, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250121/특정 위치의 문자/char-in-specific-location.py
+++ 250121/특정 위치의 문자/char-in-specific-location.py
@@ -2,9 +2,9 @@ arr = ['L', 'E', 'B', 'R', 'O', 'S']
 
 word = input()
 
-# 해당 문자가 리스트에 없는 경우
+# 해당 문자가 리스트에 없는 경우r
 if word not in arr:
     print("None")
 # 해당 문자가 리스트에 있는 경우
 else:
-    print(word.index(word))
+    print(arr.index(word))
