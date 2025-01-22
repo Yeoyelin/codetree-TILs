7b80dacdac78514929e5568cfbf2d41dbf9e62ca
Yeoyelin <yeoirene0130@gmail.com>
2025-01-22 23:46:56 +0900

    Title: 배열 놀이, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250122/배열 놀이/play-with-array.py
@@ -0,0 +1,17 @@
+n, q = input().split()
+arr = list(map(int, input().split()))
+
+for i in range(q):
+    a = list(map(int, input().split()))
+    if a[0] == 1:
+        print(arr[a[1]-1])
+    elif a[0] == 2:
+        for j in range(n):
+            if arr[j] == a[1]:
+                print(len(arr[j]))
+                break
+        else:
+            print(0)
+    elif a[0] == 3:
+        for j in range(a[1]-1, a[2]):
+            print(arr[j], end = " ")
